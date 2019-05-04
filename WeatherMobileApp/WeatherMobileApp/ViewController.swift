b34faef5200255ec9b0110cfaae5acc866b0d00d
Prashanth <pvudanti@spglobal.com>
2019-05-04 09:39:21 +0530

    Project Create/Import
    Project created and impporting basic

---
+++ WeatherMobileApp/WeatherMobileApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherMobileApp
+//
+//  Created by Prashanth on 04/05/19.
+//  Copyright © 2019 Prashanth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
