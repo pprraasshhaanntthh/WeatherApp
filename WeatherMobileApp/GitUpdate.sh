7c7a24f591f4c8daea34068ba8ca107e023a4800
Prashanth <pvudanti@spglobal.com>
2019-08-28 16:18:41 +0530

    updated script and changes
    updated

---
+++ WeatherMobileApp/GitUpdate.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+APP_TARGET="WeatherMobileApp"
+
+cd WeatherMobileApp
+
+PROJECT_FILE=$(find . -d 1 -iname '*.xcodeproj')
+
+APP_PLIST=$(
+    xcodebuild -project "$PROJECT_FILE" -target "$APP_TARGET" -showBuildSettings \
+    | grep "INFOPLIST_FILE" \
+    | awk -F  "=" '{print $2}' \
+    | awk '{$1=$1};1'
+)
+
+if [ -z "${APP_PLIST:-}" ]; then
+  echo "** ERROR: Unable to locate app info plist file"
+  exit -1
+fi
+
+BUILD_BUILDID=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" $APP_PLIST)
+
+if [[ -z "${BUILD_BUILDID:-}" ]]; then
+echo "** ERROR: Unable to determine build number."
+exit -1
+fi
+
+git add
+git commit -m"Info plist updated"
+git push
+
+echo "** INFO: Setting build number to $BUILD_BUILDID."
+exit 0
